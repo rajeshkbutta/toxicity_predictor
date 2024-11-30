9945ca8075063b88bcac7d36261c8f033d9e4db6
rajeshkbutta <114310118+rajeshkbutta@users.noreply.github.com>
2024-11-29 19:39:50 -0500

    11/29/2024 @ 19:39 - Rajesh Kumar Butta - Created initial test cases.

---
+++ test_toxicity_predictor.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+
+from toxicity_predictor import *
+import unittest
+from unittest.mock import patch
+
+class Test_toxicity_predictor(unittest.TestCase):
+    # Used for DB construction usually. Done here just to keep practice.
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    # Used for DB dismantling usually. Done here just to keep practice.
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_raw_data_retrieval(self):
+
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
